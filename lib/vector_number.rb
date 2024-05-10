ec9469a0cd2cfdb68a0a14d01d41377468f636a3
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2024-05-10 23:51:19 +0300

    Basic implementation of VectorNumber

--- lib/vector_number.rb
+++ lib/vector_number.rb
@@ -1,13 +1,82 @@
 # frozen_string_literal: true
 
 require_relative "vector_number/version"
-require_relative "vector_number/vector"
+require_relative "vector_number/querying"
 
-# A library to add together anything.
-module VectorNumber
+# A class to add together anything.
+class VectorNumber < Numeric
+  include Enumerable
+
   class Error < StandardError; end
 
+  I = 1.i
+
   def self.[](*args)
-    Vector.new(args)
+    new(args)
+  end
+
+  def initialize(array = nil)
+    super()
+    @data = Hash.new(0)
+    array&.each { |value| put_value_in_buckets(value) }
+  end
+
+  def size
+    @data.size
+  end
+
+  def each
+    return to_enum(__method__, size) unless block_given?
+
+    @data.each { |unit, value| yield [unit, value] if value.nonzero? }
+  end
+
+  alias each_pair each
+
+  protected
+
+  attr_reader :data
+
+  def data=(hash)
+    @data = hash.dup
+  end
+
+  def add_vector_to_self(vector)
+    vector.each { |unit, value| @data[unit] += value }
+  end
+
+  def type_of_self
+    :vector
+  end
+
+  private
+
+  def type_of(value)
+    case value
+    when self.class
+      value.type_of_self
+    when Complex
+      value.imaginary.nonzero? ? :complex : :real
+    when Numeric
+      value.real? ? :real : :itself
+    else
+      :itself
+    end
+  end
+
+  def put_value_in_buckets(value)
+    case type_of(value)
+    when :complex, :real
+      put_numeric_value_in_buckets(value)
+    when :vector
+      add_vector_to_self(value)
+    when :itself
+      @data[value] += 1
+    end
+  end
+
+  def put_numeric_value_in_buckets(value)
+    @data[1] += value.real
+    @data[I] += value.imaginary
   end
 end
--- spec/vector_number_spec.rb
+++ spec/vector_number_spec.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-RSpec.describe VectorNumber do
+RSpec.describe VectorNumber, :aggregate_failures do
   it "has a version number" do
     expect(VectorNumber::VERSION).not_to be_nil
   end
 
   describe ".[]" do
-    it "creates a new Vector" do
-      expect(described_class[1, 2, 3]).to be_a VectorNumber::Vector
+    subject(:number) { described_class[1, 4, "a"] }
+
+    it "creates a new VectorNumber" do
+      expect(number).to be_a described_class
+      expect(number.to_a).to eq [[1, 5], ["a", 1]]
     end
   end
 end
