a5f9659ae61b34d5975a1c8a0b4ac61fa11c23a5
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2025-06-28 15:09:59 +0300

    Add more examples

--- spec/truffelruby_spec.rb
+++ spec/truffelruby_spec.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe "Float#ceil" do
-  it "returns the ceiling of the number" do
+  it "returns the ceiling of 999.15 correctly" do
     expect(999.15.ceil).to eq 1000
     expect(999.15.ceil(1)).to eq 999.2
     expect(999.15.ceil(-1)).to eq 1000
+  end
+
+  it "returns the ceiling of 1.25 correctly" do
+    expect(1.25.ceil).to eq 2
+    expect(1.25.ceil(1)).to eq 1.3
+    expect(1.25.ceil(-1)).to eq 10
+  end
+
+  it "returns the ceiling of -999.15 correctly" do
+    expect(-999.15.ceil).to eq(-999)
+    expect(-999.15.ceil(1)).to eq(-999.1)
+    expect(-999.15.ceil(-1)).to eq(-990)
+  end
+
+  it "returns the ceiling of -1.25 correctly" do
+    expect(-1.25.ceil).to eq(-1)
+    expect(-1.25.ceil(1)).to eq(-1.2)
+    expect(-1.25.ceil(-1)).to eq(0)
   end
 end
