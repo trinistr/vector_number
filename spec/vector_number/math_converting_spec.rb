2a96cd3f846360f665e879120e17421da1ecebc9
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2025-10-08 14:19:10 +0300

    Fix #ceil tests, hopefully

--- spec/vector_number/math_converting_spec.rb
+++ spec/vector_number/math_converting_spec.rb
@@ -2,14 +2,14 @@
 
 RSpec.describe VectorNumber::MathConverting do
   let(:zero_number) { num }
-  let(:real_number) { num(999.15) }
+  let(:real_number) { num(1001.15) }
   let(:complex_number) { num(Complex(0.12, -13.5)) }
   let(:composite_number) { num("y", :a, 5, Complex(3, 5), 1.3i) }
 
   describe "#abs", :aggregate_failures do
     it "returns magnitude of the vector as a Float" do
       expect(zero_number.abs).to be 0.0
-      expect(real_number.abs).to be 999.15
+      expect(real_number.abs).to be 1001.15
       # These are exact values. Should be crossplatform too.
       expect(complex_number.abs).to be 13.50053332279877
       expect(composite_number.abs).to be 10.280564186852782
@@ -21,7 +21,7 @@ RSpec.describe VectorNumber::MathConverting do
   describe "#abs2", :aggregate_failures do
     it "returns square of the magnitude as a Float" do
       expect(zero_number.abs2).to be 0
-      expect(real_number.abs2).to be 999.15**2
+      expect(real_number.abs2).to be 1001.15**2
       expect(complex_number.abs2).to be (0.12**2) + (13.5**2)
       expect(composite_number.abs2).to be 1 + 1 + (8**2) + (6.3**2)
     end
@@ -31,7 +31,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when `digits` is 0" do
       it "truncates every coefficient to an Integer" do
         expect(zero_number.truncate).to eql num
-        expect(real_number.truncate).to eql num(999)
+        expect(real_number.truncate).to eql num(1001)
         expect(complex_number.truncate).to eql num(-13i)
         expect(composite_number.truncate).to eql num("y", :a, 8, 6i)
         expect((composite_number / 2).truncate).to eql num(4, 3i)
@@ -41,7 +41,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is > 0" do
       it "truncates excess decimal precision, rounding to zero" do
         expect(zero_number.truncate(1)).to eql num
-        expect(real_number.truncate(1)).to eql num(999.1)
+        expect(real_number.truncate(1)).to eql num(1001.1)
         expect(complex_number.truncate(1)).to eql num(0.1, -13.5i)
         expect(composite_number.truncate(1)).to eql num("y", :a, 8, 6.3i)
         expect((composite_number / 3).truncate(1).to_a).to contain_exactly(
@@ -53,7 +53,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is < 0" do
       it "truncates excess precision above 1, rounding to zero" do
         expect(zero_number.truncate(-1)).to eql num
-        expect(real_number.truncate(-1)).to eql num(990)
+        expect(real_number.truncate(-1)).to eql num(1000)
         expect(complex_number.truncate(-1)).to eql num(-10i)
         expect(composite_number.truncate(-1)).to eql num
         expect((composite_number * 3).truncate(-1)).to eql num(20, 10i)
@@ -65,7 +65,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when `digits` is 0" do
       it "rounds every coefficient up to an Integer" do
         expect(zero_number.ceil).to eql num
-        expect(real_number.ceil).to eql num(1000)
+        expect(real_number.ceil).to eql num(1002)
         expect(complex_number.ceil).to eql num(1, -13i)
         expect(composite_number.ceil).to eql num("y", :a, 8, 7i)
         expect((composite_number / 2).ceil).to eql num("y", :a, 4, 4i)
@@ -75,7 +75,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is > 0" do
       it "truncates excess decimal precision, rounding up" do
         expect(zero_number.ceil(1)).to eql num
-        expect(real_number.ceil(1)).to eql num(999.2)
+        expect(real_number.ceil(1)).to eql num(1001.2)
         expect(complex_number.ceil(1)).to eql num(0.2, -13.5i)
         expect(composite_number.ceil(1)).to eql num("y", :a, 8, 6.3i)
         expect((composite_number / 3).ceil(1).to_a).to contain_exactly(
@@ -87,7 +87,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is < 0" do
       it "truncates excess precision above 1, rounding up" do
         expect(zero_number.ceil(-1)).to eql num
-        expect(real_number.ceil(-1)).to eql num(1000)
+        expect(real_number.ceil(-1)).to eql num(1010)
         expect(complex_number.ceil(-1)).to eql num(10, -10i)
         expect(composite_number.ceil(-1).to_a).to contain_exactly(
           ["y", 10], [:a, 10], [VectorNumber::R, 10], [VectorNumber::I, 10]
@@ -103,7 +103,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when `digits` is 0" do
       it "rounds every coefficient down to an Integer" do
         expect(zero_number.floor).to eql num
-        expect(real_number.floor).to eql num(999)
+        expect(real_number.floor).to eql num(1001)
         expect(complex_number.floor).to eql num(-14i)
         expect(composite_number.floor).to eql num("y", :a, 8, 6i)
         expect((composite_number / 2).floor).to eql num(4, 3i)
@@ -113,7 +113,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is > 0" do
       it "truncates excess decimal precision, rounding down" do
         expect(zero_number.floor(1)).to eql num
-        expect(real_number.floor(1)).to eql num(999.1)
+        expect(real_number.floor(1)).to eql num(1001.1)
         expect(complex_number.floor(1)).to eql num(0.1, -13.5i)
         expect(composite_number.floor(1)).to eql num("y", :a, 8, 6.3i)
         expect((composite_number / 3).floor(1).to_a).to contain_exactly(
@@ -125,7 +125,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is < 0" do
       it "truncates excess precision above 1, rounding down" do
         expect(zero_number.floor(-1)).to eql num
-        expect(real_number.floor(-1)).to eql num(990)
+        expect(real_number.floor(-1)).to eql num(1000)
         expect(complex_number.floor(-1)).to eql num(-20i)
         expect(composite_number.floor(-1)).to eql num
         expect((composite_number * 3).floor(-1)).to eql num(20, 10i)
@@ -137,7 +137,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when `digits` is 0" do
       it "rounds every coefficient to an Integer, rounding up on half" do
         expect(zero_number.round).to eql num
-        expect(real_number.round).to eql num(999)
+        expect(real_number.round).to eql num(1001)
         expect(complex_number.round).to eql num(-14i)
         expect(composite_number.round).to eql num("y", :a, 8, 6i)
         expect((composite_number / 2).round).to eql num("y", :a, 4, 3i)
@@ -147,7 +147,7 @@ RSpec.describe VectorNumber::MathConverting do
     context "when digits is > 0" do
       it "truncates excess decimal precision, rounding up on half" do
         expect(zero_number.round(1)).to eql num
-        expect(real_number.round(1)).to eql num(999.2)
+        expect(real_number.round(1)).to eql num(1001.2)
         expect(complex_number.round(1)).to eql num(0.1, -13.5i)
         expect(composite_number.round(1)).to eql num("y", :a, 8, 6.3i)
         expect((composite_number / 3).round(1).to_a).to contain_exactly(
